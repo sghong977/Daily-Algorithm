67b2937c942686ca264bc948a7cf47225d90c7e0
SeulGi Hong <46152199+sghong977@users.noreply.github.com>
2022-09-13 19:34:30 +0900

    Create even_odd.py

---
+++ 연습문제/even_odd.py
@@ -0,0 +1,11 @@
+# LV.1
+# 틀리면 멍청이
+def solution(num):
+    answer = ''
+    if num == 0:
+        answer = "Even"
+    elif num % 2 ==0:
+        answer = "Even"
+    else:
+        answer = "Odd"
+    return answer
